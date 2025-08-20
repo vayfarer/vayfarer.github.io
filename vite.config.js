7b74f210f5c2e6be6dec2b35a19254d755e85e2b
Mike <mrc22091@yahoo.com>
2025-08-20 13:07:27 -0700

    Troubleshooting. Change base path to /

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vayfarer.github.io/', // repo name
+  base: '/', // base path '/' for user sites. 
 })
